f1ed20b3260d053e911a0cd826384d5568c10397
Gaspard Grandidier <gaspard.grandidier@etudiant.univ-lr.fr>
2025-06-06 19:33:46 +0200

    Change the link for distribution

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://mcflowblock.com/storage/nebula/distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://mcflowblock.com/storage/nebula/mcflowblock-distro.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
